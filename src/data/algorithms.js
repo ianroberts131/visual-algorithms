1a5826e6df7dd2d440af28aa092ead7369fbff5e
Ian Roberts <ianroberts131@gmail.com>
2017-08-29 05:48:27 -0700

    Fix complexity analysis display to show superscripts

--- src/data/algorithms.js
+++ src/data/algorithms.js
@@ -14,25 +14,25 @@ export var selectionSort = {
   name: "Selection Sort",
   url: "selection-sort",
   description: "An in-place sorting algorithm that divides a collection into two parts: a sublist of sorted elements and a sublist of unsorted elements. Initially, the sorted sublist is empty. The algorithm proceeds to find the smallest element in the unsorted sublist, and swaps it with the leftmost unsorted element.",
-  worstCase: "O(n2)",
-  bestCase: "O(n2)",
-  averageCase: "O(n2)"
+  worstCase: "O(n<sup>2</sup>)",
+  bestCase: "O(n<sup>2</sup>)",
+  averageCase: "O(n<sup>2</sup>)"
 }
 
 export var insertionSort = {
   name: "Insertion Sort",
   url: "insertion-sort",
   description: "An in-place sorting algorithm that divides a collection into two parts: a sublist of sorted elements and a sublist of unsorted elements. Intially, the sorted sublist is empty. The algoirthm procees to take each element and insert it into its proper position of the sorted sublist.",
-  worstCase: "O(n2)",
+  worstCase: "O(n<sup>2</sup>)",
   bestCase: "O(n)",
-  averageCase: "O(n2)"
+  averageCase: "O(n<sup>2</sup>)"
 }
 
 export var bubbleSort = {
   name: "Bubble Sort",
   url: "bubble-sort",
   description: "An in-place sorting algorithm that repeatedly steps through the collection to be sorted, compares each pair of adjacent items, and swaps them if they are in the wrong order. The pass through the collection is repeated until no swaps are needed, which indicates that the collection is sorted.",
-  worstCase: "O(n2)",
+  worstCase: "O(n<sup>2</sup>)",
   bestCase: "O(n)",
-  averageCase: "O(n2)"
+  averageCase: "O(n<sup>2</sup>)"
 }
