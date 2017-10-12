9568063b87baba01eddef398cc02430359682376
Ian Roberts <ianroberts131@gmail.com>
2017-10-11 21:06:30 -0700

    Make homepage responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,35 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { Row, Col } from 'react-bootstrap';
 
 class Home extends Component {
   render() {
     return (
-      <div className="home">
-        <h1 id="home-header">Visual Algorithms</h1>
-        <h3 id="home-description">Bringing common computer science algorithms to life</h3>
-        <div id="algo-boxes">
-          <div className='algo-item'>
+      <Row className="home">
+        <Col xs={ 12 } id="header-section">
+          <h1 id="home-header">Visual Algorithms</h1>
+          <h3 id="home-description">Bringing common computer science algorithms to life</h3>
+        </Col>
+        <Col xs={ 12 } id="algo-boxes">
+          <Col xs={ 12 } sm={ 6 } className='algo-item'>
             <Link className="link" to="/search">
-              <h3>Searching Algorithms</h3>
+              <h3 className="algo-header">Searching Algorithms</h3>
               <img className='search-img' src={require('../images/search-img.jpg')} alt="Searching Algorithm" onClick={ (e) => {
                   this.props.searchBaseState();
                   this.props.changeSpeed('regular');
                 }
               }></img>
             </Link>
-          </div>
-          <div className='algo-item'>
+          </Col>
+          <Col xs={ 12 } sm={ 6 } className='algo-item'>
             <Link className="link" to="/sort">
-              <h3>Sorting Algorithms</h3>
+              <h3 className="algo-header">Sorting Algorithms</h3>
               <img className='sort-img' src={require('../images/sort-img.jpg')} alt="Sorting Algorithm" onClick={ (e) => {
                   this.props.sortBaseState();
                   this.props.changeSpeed('regular');
                 }
               }></img>
             </Link>
-          </div>
-        </div>
-      </div>
+          </Col>
+        </Col>
+      </Row>
     )
   }
 }
