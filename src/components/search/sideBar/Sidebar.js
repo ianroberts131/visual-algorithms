b114dca2da7454448fb7a47e0fa034b17087b6e9
Bailey <bailey.g.weathers@gmail.com>
2017-10-30 18:00:40 -0500

    Complete add complexity analysis to search page

--- src/components/search/sideBar/Sidebar.js
+++ src/components/search/sideBar/Sidebar.js
@@ -21,6 +21,16 @@ class Sidebar extends React.Component {
               <p className="iteration-info-item">Item located at index <span>{ index }</span></p>
             </div>
           </Col>
+          <Col xs={ 6 } sm={ 12 } className="iteration-info-container">
+            <div>
+              <p id="complexity-title" className="header">Complexity:</p>
+            </div>
+            <div className="iteration-info">
+              <p className="complexity-detail" dangerouslySetInnerHTML={{__html: "Worst Case: " + searchAlgorithm.worstCase}}></p>
+              <p className="complexity-detail" dangerouslySetInnerHTML={{__html: "Best Case: " + searchAlgorithm.bestCase}}></p>
+              <p className="complexity-detail" dangerouslySetInnerHTML={{__html: "Average Case: " + searchAlgorithm.averageCase}}></p>
+            </div>
+          </Col>
         </Col>
     )
   }
