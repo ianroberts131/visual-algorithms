7f1cc148ec23354aa0baf62d135191cb4f7bfe06
Bailey <bailey.g.weathers@gmail.com>
2017-11-10 09:48:18 -0500

    Add structure

---
+++ src/actions/action_test.js
@@ -0,0 +1,12 @@
+/*
+import * as actionCreators from '../../actions/actionCreators'
+
+describe('actionCreators', () => {
+  it('Should Create an Action to searchBaseState', () => {
+    const expectedAction = {
+      type: types.SEARCH_BASE_STATE,
+    }
+    expect(actions.searchBaseState.toEqual(expectedAction)
+  })
+})
+*/
---
+++ test.js
@@ -0,0 +1,11 @@
+
+import * as actions from './src/actions/actionCreators';
+
+/*
+describe('actionCreators', () => {
+  it('Should Create an Action to searchBaseState', () => {
+    expect(actionCreators.sortBaseState).toEqual({
+      type: types.SORT_BASE_STATE
+    })
+  })
+*/
