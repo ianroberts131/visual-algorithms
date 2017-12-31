ce9b8ac7bb620cb75533c8f3f4001849679765d5
Bailey <bailey.g.weathers@gmail.com>
2017-12-31 09:15:55 -0500

    Complete refactor actionCreator separation

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -10,19 +10,9 @@ import * as  actionMergeSort from './actionMergeSort';
 import * as  actionQuickSort from './actionQuickSort';
 import * as  actionSelectionSort from './actionSelectionSort';
 
-
 let actionObj = {};
 
-actionObj = Object.assign(actionObj, actionBaseSet);
-actionObj = Object.assign(actionObj, actionBinarySearch);
-actionObj = Object.assign(actionObj, actionBreadthFirstSearch);
-actionObj = Object.assign(actionObj, actionLinearSearch);
-actionObj = Object.assign(actionObj, actionDepthFirstSearch);
-actionObj = Object.assign(actionObj, actionBubbleSort);
-actionObj = Object.assign(actionObj, actionHeapSort);
-actionObj = Object.assign(actionObj, actionInsertionSort);
-actionObj = Object.assign(actionObj, actionMergeSort);
-actionObj = Object.assign(actionObj, actionQuickSort);
-actionObj = Object.assign(actionObj, actionSelectionSort);
+actionObj = Object.assign(actionObj, actionBaseSet, actionBinarySearch, actionBreadthFirstSearch, actionLinearSearch, actionDepthFirstSearch,
+  actionBubbleSort, actionHeapSort, actionInsertionSort, actionMergeSort, actionQuickSort, actionSelectionSort );
 
 export const actionCreators = Object.assign({}, actionObj);
