24da0fc0714fa9c47a4026c44b692b64d80b4713
Bailey <bailey.g.weathers@gmail.com>
2018-01-12 13:43:35 -0500

    Complete code update and refactor

--- src/components/sort/heapSort/HeapSort.js
+++ src/components/sort/heapSort/HeapSort.js
@@ -8,8 +8,11 @@ class HeapSort extends React.Component {
   render() {
     const { heapSortArray, heapTreeTop, heapTreeRow2, heapTreeRow3, heapTreeRow4, heapTreeArray, heapFinalSort } = this.props.sort;
     const hideCell =  "heap-sort-box-level-0 heap-hidden";
-    heapTreeTop[5] = heapTreeArray[0], heapTreeRow2[2] = heapTreeArray[1], heapTreeRow2[8] = heapTreeArray[2], heapTreeRow3[1] = heapTreeArray[3], heapTreeRow3[3] = heapTreeArray[4];
-    heapTreeRow3[7] = heapTreeArray[5], heapTreeRow3[9] = heapTreeArray[6], heapTreeRow4[0] = heapTreeArray[7], heapTreeRow4[1] = heapTreeArray[8], heapTreeRow4[3] = heapTreeArray[9];
+    heapTreeTop[5] = heapTreeArray[0], heapTreeRow2[2] = heapTreeArray[1],
+    heapTreeRow2[8] = heapTreeArray[2], heapTreeRow3[1] = heapTreeArray[3],
+    heapTreeRow3[3] = heapTreeArray[4], heapTreeRow3[7] = heapTreeArray[5],
+    heapTreeRow3[9] = heapTreeArray[6], heapTreeRow4[0] = heapTreeArray[7],
+    heapTreeRow4[1] = heapTreeArray[8], heapTreeRow4[3] = heapTreeArray[9];
 
     return (
       <div className="heap-sort-area">
